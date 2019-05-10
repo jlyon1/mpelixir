988f245e3a01a20287c5760d5832f875d44ff303
Joseph Lyon <joeylyon1@gmail.com>
2019-05-10 16:03:02 -0400

    add find_and_pop and random_pushback

--- lib/mprouter.ex
+++ lib/mprouter.ex
@@ -1,22 +1,53 @@
 defmodule MP.TaskRouter do
-  def route(_mod, _fun, _args) do
+  def find_and_pop() do
     case MP.Queue.len do
         {:ok, 0} ->
-           find_good_node table()
-
+            case find_good_node table() do
+                {:empty, e} ->
+                    {:empty, e}
+                {:ok, val} ->
+                    task = Task.Supervisor.async {MP.Tasks, val}, MP.Queue, :pop, []
+                    task
+                    |> Task.await
+            end
         _ -> 
-            node()
+            task = Task.Supervisor.async {MP.Tasks, node()}, MP.Queue, :pop, []
+        task
+        |> Task.await
+    end
+  end
+
+  def rand_push(val) do
+    tbl = rand_node(table(), -1)
+    task = Task.Supervisor.async {MP.Tasks, tbl}, MP.Queue, :pushback, [val]
+    task
+    |> Task.await
+  end
+
+  def rand_node(table, i) do
+    case i do
+        -1 -> 
+            rand_node(table, :rand.uniform(length(table)) - 1)
+        0 -> 
+            hd table
+        _ ->
+            rand_node(tl(table), (i - 1))
+    
     end
   end
 
   # returns the node with at least one element in its queue
   def find_good_node(table) do
-    task = Task.Supervisor.async {MP.Tasks, hd table}, MP.Queue, :len, []
-    case Task.await(task) do
-        {:ok, 0} -> 
-            find_good_node(tl table)
-        _ -> 
-            hd table
+    if length(table) == 0 do
+        {:empty, :empty}        
+    else
+        task = Task.Supervisor.async {MP.Tasks, hd table}, MP.Queue, :len, []
+        case Task.await(task) do
+            {:ok, 0} -> 
+                find_good_node(tl table)
+            _ -> 
+                {:ok, hd table}
+        end
     end
   end
 
